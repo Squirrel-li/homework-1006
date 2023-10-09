ebccf30bbe93dfdd8bcb92b1b524c24b2a23d23d
Squirrel-li <lisong20050429@gmail.com>
2023-10-09 16:23:52 +0800

    2.33

--- code/2.33/source/main.c
+++ code/2.33/source/main.c
@@ -4,15 +4,24 @@
 
 int main()
 {
-	puts("*********         ***           *           *         ");
-	puts("*       *      *       *       ***         * *        ");
-	puts("*       *     *         *     *****       *   *       ");
-	puts("*       *     *         *       *        *     *      ");
-	puts("*       *     *         *       *       *       *     ");
-	puts("*       *     *         *       *        *     *      ");
-	puts("*       *     *         *       *         *   *       ");
-	puts("*       *     *         *       *          * *        ");
-	puts("*********         ***           *           *         ");
-	system("pause");
+	float miles, cost, distance, fee, tolls;
+	float totalcost;
+
+	printf("Please enter your total miles driven per day:");
+	scanf("%f", &miles);
+	printf("Please enter your cost per liter of gasoline:");
+	scanf("%f", &cost);
+	printf("Please enter your average miles per liter:");
+	scanf("%f", &distance);
+	printf("Please enter your parking fees per day:");
+	scanf("%f", &fee);
+	printf("Please enter your tolls per day:");
+	scanf("%f", &tolls);
+
+	totalcost = (miles / distance)*cost + fee + tolls;
+
+	printf("your total cost per day of driving to work is %1.f\n", totalcost);
+
+	system("Pause");
 	return 0;
 }
